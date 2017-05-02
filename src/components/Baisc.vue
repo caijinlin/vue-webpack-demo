64676c503a27a486f3538498efeeb3262380b8ad
蔡金林 <caijinlin@justering.com>
2017-05-02 23:13:15 +0800

    ADD sub routes

---
+++ src/components/Baisc.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <p>{{ address }}</p>
+  <input v-model="address">
+</div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        address: ''
+      }
+    }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Basic from '@/components/Baisc'
 
 Vue.use(Router)
 
@@ -10,6 +11,15 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/basic',
+      name: 'Basic',
+      component: Basic,
+      children: [{
+        path: 'index',
+        component: Basic
+      }]
     }
   ]
 })
